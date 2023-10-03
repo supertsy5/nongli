193a6e202a13d957a61ea009b0b34d89c8cd756d
0x7f178735 <0x7f178735@gmail.com>
2023-10-03 19:44:19 +0800

    20231003

---
+++ src/lib.rs
@@ -0,0 +1,148 @@
+use Day::*;
+use Month::*;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Day {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Month {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December,
+}
+
+pub struct Date {
+    year: u16,
+    month: Month,
+    day: u8,
+}
+
+pub const DAYS: [Day; 7] = [
+    Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday,
+];
+pub const MONTHS: [Month; 12] = [
+    January, February, March, April, May, June, July, August, September, October, November,
+    December,
+];
+pub const UNIX_EPOCH: Date = Date {
+    year: 1970,
+    month: January,
+    day: 1,
+};
+pub const DAY_OF_UNIX_EPOCH: Day = Thursday;
+
+pub fn is_leap_year(year: u16) -> bool {
+    year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+}
+
+pub fn days_of_year(year: u16) -> u16 {
+    if is_leap_year(year) {
+        366
+    } else {
+        365
+    }
+}
+
+pub fn days_of_month(year: u16, month: Month) -> u8 {
+    match month {
+        January => 31,
+        February => {
+            if is_leap_year(year) {
+                29
+            } else {
+                28
+            }
+        }
+        March => 31,
+        April => 30,
+        May => 31,
+        June => 30,
+        July => 31,
+        August => 31,
+        September => 30,
+        October => 31,
+        November => 30,
+        December => 31,
+    }
+}
+
+impl Month {
+    pub fn from_number(number: u8) -> Option<Month> {
+        number
+            .checked_sub(1)
+            .and_then(|number| MONTHS.get(number as usize))
+            .copied()
+    }
+    pub fn as_number(self) -> u8 {
+        self as u8 + 1
+    }
+}
+
+impl Day {
+    pub fn from_number(number: u8) -> Option<Day> {
+        if number == 7 {
+            Some(Sunday)
+        } else {
+            DAYS.get(number as usize).copied()
+        }
+    }
+    pub fn as_number(self) -> u8 {
+        self as u8
+    }
+    pub fn offset(self, offset: i32) -> Day {
+        DAYS[(self as i32 + offset.rem_euclid(7)).rem_euclid(7) as usize]
+    }
+}
+
+impl Date {
+    pub fn new(year: u16, month: Month, day: u8) -> Option<Self> {
+        (year >= 1970 && day <= days_of_month(year, month)).then_some(Self { year, month, day })
+    }
+    pub fn year(&self) -> u16 {
+        self.year
+    }
+    pub fn month(&self) -> Month {
+        self.month
+    }
+    pub fn day(&self) -> u8 {
+        self.day
+    }
+    pub fn day_of_week(&self) -> Day {
+        DAY_OF_UNIX_EPOCH
+            .offset((1970..self.year).map(days_of_year).sum::<u16>() as i32)
+            .offset(
+                (1..self.month.as_number())
+                    .map(|month| {
+                        days_of_month(self.year, Month::from_number(month).unwrap()) as i32
+                    })
+                    .sum::<i32>(),
+            )
+            .offset(self.day as i32 - 1)
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn tests() {
+    assert_eq!(UNIX_EPOCH.day_of_week(), Thursday);
+    assert_eq!(Date::new(1970, February, 1).unwrap().day_of_week(), Sunday);
+    assert_eq!(Date::new(1971, January, 1).unwrap().day_of_week(), Friday);
+    assert_eq!(Date::new(2023, October, 3).unwrap().day_of_week(), Tuesday);
+}
