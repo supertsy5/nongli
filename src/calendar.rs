88d0f81c107d17206c189492eed4618747c40ab4
SUPERTSY5 <0x7f178735@gmail.com>
2024-12-24 19:55:32 +0800

    Added the new `Calendar` system

--- src/calendar.rs
+++ src/calendar.rs
@@ -1,5 +1,5 @@
-use crate::language::Language;
-use chrono::{Month, NaiveDate};
+use crate::{days_of_month, language::Language};
+use chrono::{Datelike, Month, NaiveDate, Weekday};
 
 #[derive(Clone, Copy, Debug)]
 pub struct Options {
@@ -18,6 +18,12 @@ pub struct Calendar {
     pub options: Options,
 }
 
+#[derive(Clone, Debug)]
+pub struct Iter<'a> {
+    calendar: &'a Calendar,
+    day: u8,
+}
+
 impl Calendar {
     pub fn pred(mut self) -> Self {
         if self.month == Month::January {
@@ -33,4 +39,108 @@ impl Calendar {
         self.month = self.month.succ();
         self
     }
+    pub fn iter<'a>(&'a self) -> Iter<'a> {
+        Iter {
+            calendar: self,
+            day: 1,
+        }
+    }
+}
+
+impl Iterator for Iter<'_> {
+    type Item = [Option<NaiveDate>; 7];
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.day >= days_of_month(self.calendar.year, self.calendar.month) {
+            return None;
+        }
+        let start_on_monday = self.calendar.options.start_on_monday;
+        let (start_of_week, end_of_week) = if start_on_monday {
+            (Weekday::Mon, Weekday::Sun)
+        } else {
+            (Weekday::Sun, Weekday::Sat)
+        };
+        let mut array = [Option::<NaiveDate>::None; 7];
+        while self.day <= days_of_month(self.calendar.year, self.calendar.month) {
+            let date = NaiveDate::from_ymd_opt(
+                self.calendar.year as i32,
+                self.calendar.month.number_from_month(),
+                self.day as u32,
+            )
+            .unwrap();
+            let weekday = date.weekday();
+            array[weekday.days_since(start_of_week) as usize] = Some(date);
+            self.day += 1;
+            if weekday == end_of_week {
+                break;
+            }
+        }
+        Some(array)
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn test() {
+    let calendar = Calendar {
+        year: 2025,
+        month: Month::January,
+        today: NaiveDate::from_ymd_opt(2025, 1, 1).unwrap(),
+        options: Options {
+            color: false,
+            enable_chinese: false,
+            language: Language::English,
+            highlight_today: false,
+            start_on_monday: false,
+        },
+    };
+    assert_eq!(
+        calendar.iter().collect::<Vec<_>>(),
+        &[
+            [
+                None,
+                None,
+                None,
+                NaiveDate::from_ymd_opt(2025, 1, 1),
+                NaiveDate::from_ymd_opt(2025, 1, 2),
+                NaiveDate::from_ymd_opt(2025, 1, 3),
+                NaiveDate::from_ymd_opt(2025, 1, 4),
+            ],
+            [
+                NaiveDate::from_ymd_opt(2025, 1, 5),
+                NaiveDate::from_ymd_opt(2025, 1, 6),
+                NaiveDate::from_ymd_opt(2025, 1, 7),
+                NaiveDate::from_ymd_opt(2025, 1, 8),
+                NaiveDate::from_ymd_opt(2025, 1, 9),
+                NaiveDate::from_ymd_opt(2025, 1, 10),
+                NaiveDate::from_ymd_opt(2025, 1, 11),
+            ],
+            [
+                NaiveDate::from_ymd_opt(2025, 1, 12),
+                NaiveDate::from_ymd_opt(2025, 1, 13),
+                NaiveDate::from_ymd_opt(2025, 1, 14),
+                NaiveDate::from_ymd_opt(2025, 1, 15),
+                NaiveDate::from_ymd_opt(2025, 1, 16),
+                NaiveDate::from_ymd_opt(2025, 1, 17),
+                NaiveDate::from_ymd_opt(2025, 1, 18),
+            ],
+            [
+                NaiveDate::from_ymd_opt(2025, 1, 19),
+                NaiveDate::from_ymd_opt(2025, 1, 20),
+                NaiveDate::from_ymd_opt(2025, 1, 21),
+                NaiveDate::from_ymd_opt(2025, 1, 22),
+                NaiveDate::from_ymd_opt(2025, 1, 23),
+                NaiveDate::from_ymd_opt(2025, 1, 24),
+                NaiveDate::from_ymd_opt(2025, 1, 25),
+            ],
+            [
+                NaiveDate::from_ymd_opt(2025, 1, 26),
+                NaiveDate::from_ymd_opt(2025, 1, 27),
+                NaiveDate::from_ymd_opt(2025, 1, 28),
+                NaiveDate::from_ymd_opt(2025, 1, 29),
+                NaiveDate::from_ymd_opt(2025, 1, 30),
+                NaiveDate::from_ymd_opt(2025, 1, 31),
+                None,
+            ]
+        ]
+    );
 }
